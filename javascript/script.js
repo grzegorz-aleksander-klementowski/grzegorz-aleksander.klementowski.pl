ec65a9fb7baa788b3eb41759e57199e67ace0c42
Grzegorz Aleksander Klementowski <125459323+Klemens-Gak@users.noreply.github.com>
2023-11-05 20:51:23 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,36 +1,3 @@
-/*
-// Funkcja callback, która zostanie wywołana, kiedy obserwowany element
-// wejdzie lub wyjdzie z viewportu
-const callback = (entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      // Element jest widoczny na ekranie
-      console.log('Element jest widoczny');
-    } else {
-      // Element nie jest widoczny na ekranie
-      console.log('Element nie jest widoczny');
-    }
-  });
-};
-
-// Stworzenie instancji IntersectionObserver
-const observer = new IntersectionObserver(callback);
-
-// Znalezienie elementu, który chcemy obserwować
-const elementDoObserwacji = document.querySelector('.curriculum-vitae');
-
-// Rozpoczęcie obserwacji elementu
-observer.observe(elementDoObserwacji);
-
-*/
-
-
-
-// Można wywołać tę funkcję w odpowiednim miejscu, na przykład po załadowaniu strony lub po zdarzeniu scrollowania
-// window.onload = findCloserElement;
-//window.onscroll = findCloserElement;
-
-
 
 
 
@@ -43,56 +10,129 @@ function scrollToClass(className) {
     }
   }
 
+
+
+
+
+
+// Tablica z klasami w odpowiedniej kolejności
+const sectionClasses = [
+  'header-index',
+  'main-presentation',
+  'hero-skills',
+  'curriculum-vitae',
+  'footer-index'
+];
+
+// Funkcja do przewijania do danej klasy
+function scrollToClass(className) {
+  const element = document.querySelector('.' + className);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+// Funkcja do znajdowania aktualnej sekcji
+function getCurrentSectionIndex() {
+  const scrollPosition = window.pageYOffset;
+  const sections = sectionClasses.map(className => document.querySelector('.' + className));
+
+  let closestSectionIndex = 0;
+  let smallestDistance = Infinity;
+
+  for (let i = 0; i < sections.length; i++) {
+    const rect = sections[i].getBoundingClientRect();
+    const sectionTop = rect.top + scrollPosition;
+    const distance = Math.abs(scrollPosition - sectionTop);
+
+    if (distance < smallestDistance) {
+      smallestDistance = distance;
+      closestSectionIndex = i;
+    }
+  }
+
+  return closestSectionIndex;
+}
+
 // Funkcja do obsługi zdarzenia przewijania kółkiem myszy
 const onWheelEvent = (event) => {
-    // `deltaY` wskazuje kierunek przewijania:
-    // dodatni wartości `deltaY` oznaczają przewijanie w dół
-    if (event.deltaY > 0) {
-      console.log('Przewijanie w dół');
-      //scrollToClass('main-presentation') przesuń do klasy .main-presentation
-      // Tutaj możesz wywołać funkcję, która obsługuje przewijanie do następnej sekcji
-    } else {
-      console.log('Przewijanie w górę');
-      // Funkcja do przewijania do poprzedniej sekcji
+  // Pobranie aktualnego indeksu sekcji
+  let currentSectionIndex = getCurrentSectionIndex();
+
+  // `deltaY` wskazuje kierunek przewijania:
+  // dodatni wartości `deltaY` oznaczają przewijanie w dół
+  if (event.deltaY > 0) {
+    console.log('Przewijanie w dół');
+    // Przesuń do klasy pod aktualnym indeksem, o ile nie jest to ostatnia sekcja
+    if (currentSectionIndex < sectionClasses.length - 1) {
+      scrollToClass(sectionClasses[currentSectionIndex + 1]);
     }
-  };
+  } else {
+    console.log('Przewijanie w górę');
+    // Przesuń do klasy nad aktualnym indeksem, o ile nie jest to pierwsza sekcja
+    if (currentSectionIndex > 0) {
+      scrollToClass(sectionClasses[currentSectionIndex - 1]);
+    }
+  }
+};
+
+// Dodanie nasłuchiwania na zdarzenie przewijania kółkiem
+document.addEventListener('wheel', onWheelEvent);
+
   
-  // Dodanie nasłuchiwanie na zdarzenie przewijania kółkiem
-  document.addEventListener('wheel', onWheelEvent);
 
 
-
-  
 
 
 //funkcja, która sprawdza, czyjesteśmy bliżej klasy .A, czy klasy .B.
-function findCloserElement(classNameA, classNameB) {
+function scrollToClass(className) {
+  // Logika przewijania do danej klasy
+  console.log('Przewijanie do:', className);
+  // Przykładowo, użyjemy 'scrollIntoView' dla elementu
+  const element = document.querySelector('.' + className);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+// Funkcja, która sprawdza, który element jest bliżej środka ekranu.
+function findCloserElement() {
   const windowHeight = window.innerHeight;
   const centerOfWindow = windowHeight / 2;
 
   // Pobranie elementów
-  const elementA = document.querySelector(classNameA);
-  const elementB = document.querySelector(classNameB);
-
+  const elementA = document.querySelector('.header-index');
+  const elementB = document.querySelector('.main-presentation');
+  const elementC = document.querySelector('.hero-skills');
+  const elementD = document.querySelector('.curriculum-vitae');
+  const elementE = document.querySelector('.footer-index');
 
   // Obliczenie odległości od środka ekranu do środka każdego elementu
-  const rectA = elementA.getBoundingClientRect();
-  const centerOfElementA = rectA.top + (rectA.height / 2);
-  const distanceA = Math.abs(centerOfWindow - centerOfElementA);
+  const calculateDistance = (element) => {
+    const rect = element.getBoundingClientRect();
+    const centerOfElement = rect.top + (rect.height / 2);
+    return Math.abs(centerOfWindow - centerOfElement);
+  };
 
-  const rectB = elementB.getBoundingClientRect();
-  const centerOfElementB = rectB.top + (rectB.height / 2);
-  const distanceB = Math.abs(centerOfWindow - centerOfElementB);
+  // Obliczenie odległości dla każdego elementu
+  const distances = [
+    { element: 'header-index', distance: calculateDistance(elementA) },
+    { element: 'main-presentation', distance: calculateDistance(elementB) },
+    { element: 'hero-skills', distance: calculateDistance(elementC) },
+    { element: 'curriculum-vitae', distance: calculateDistance(elementD) },
+    { element: 'footer-index', distance: calculateDistance(elementE) }
+  ];
 
-  // Porównanie odległości i zwrócenie elementu bliższego środkowi ekranu
-  if (distanceA < distanceB) {
-    console.log('Element A jest bliżej środka wysokości ekranu');
-  } else if (distanceB < distanceA) {
-    console.log('ElementB jest bliżej środka wysokości ekranu');
-  } else {
-    console.log('Oba elementy są w jednakowej odległości od środka wysokości ekranu');
-  }
+  // Posortuj elementy według odległości od centrum
+  distances.sort((a, b) => a.distance - b.distance);
+
+  // Zwróć klasę elementu najbliższego środkowi ekranu
+  const closest = distances[0].element;
+
+  console.log(`${closest} jest najbliżej środka wysokości ekranu`);
+  scrollToClass(closest);
 }
+
   
 
 // Zmienna stanu, czy przeciąganie jest aktywne
@@ -121,7 +161,7 @@ window.addEventListener('mouseup', function(event) {
     //console.log('Suwak został puszczony po przesunięciu');
     isDragging = false; // Zresetuj stan przeciągania
 
-    findCloserElement('.header-index', '.main-presentation');
+    findCloserElement();
 
   }
 });
